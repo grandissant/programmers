949a591f27875a87b7a97db2cdd0ff0ef842fdcf
grandissant <160201222+grandissant@users.noreply.github.com>
2024-02-17 14:03:55 +0900

    Update programmers_string_len.py

--- programmers_string_len.py
+++ programmers_string_len.py
@@ -1,6 +1,6 @@
 
-def solution(s):
-    if (len(s) == 4 or len(s) == 6) and s.isdigit():
+def solution(s): #문자열의 길이가 4 또는 6이고 숫자로 이루어져있는지 확인하는 함수
+    if (len(s) == 4 or len(s) == 6) and s.isdigit():#isdigit함수 숫자로만 이루어져있는지 판별
         return True
     else:
         return False
