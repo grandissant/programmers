58cee272d0d69da404c23dd4f565b1d5a88a8677
grandissant <160201222+grandissant@users.noreply.github.com>
2024-03-15 13:04:26 +0900

    Add files via upload

---
+++ programmers_print.py
@@ -0,0 +1,10 @@
+string_msg = 'Spring is beginning'#문자 ""이나''
+
+int_val = 3 #정수
+
+string_val = '3' #정수를 문자로 ""이나 ''
+
+
+print(string_msg) #문자 출력
+print(int_val + 10) #정수 + 정수
+print(string_val + "10") # 문자변환 + 문자
